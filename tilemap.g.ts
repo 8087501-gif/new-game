5f5b276ca58dad9261918c2662dd16bcd56b8252
8087501-gif <8087501@student.harmonytx.org>
2025-09-19 10:35:23 -0500

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,42 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010202020202020202020202020202010102020202020202020202020202020101020202010202010202020102020201010202020202020202020202020202010102020202020102020102020102020101020202020202020202020202020201010202020202020101020202020202010102020201020201010202020202020101020202020202020202020202020201010202020202020201020202020202010102010202010202020202020102020101020202020202020202020202020201010201020202020202020202020202010102020202020201020202010202020101010101010101010101010101010101`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . 2 . . 2 . . . 2 . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . 2 . . 2 . . 2 . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . 2 2 . . . . . . 2 
+2 . . . 2 . . 2 2 . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . 2 . . . . . . 2 
+2 . 2 . . 2 . . . . . . 2 . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . 2 . . . . . . . . . . . . 2 
+2 . . . . . . 2 . . . 2 . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.builtin.forestTiles0,sprites.castle.tilePath5], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "tile1":return tile1;
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
